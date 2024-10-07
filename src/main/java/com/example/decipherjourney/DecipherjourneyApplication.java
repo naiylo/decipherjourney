6ded59f1c00130cac72e20fc021b47b40c1cadfc
Oskar Schiedewitz <schiedewitz1@uni-potsdam.de>
2024-10-07 08:29:41 +0200

    Initial commit

---
+++ src/main/java/com/example/decipherjourney/DecipherjourneyApplication.java
@@ -0,0 +1,13 @@
+package com.example.decipherjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecipherjourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecipherjourneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/decipherjourney/DecipherjourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.decipherjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecipherjourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
