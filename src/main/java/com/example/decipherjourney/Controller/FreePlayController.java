d78dc0576170d830a84c1994d93a026ea1468ea1
Oskar Schiedewitz <schiedewitz1@uni-potsdam.de>
2024-11-01 11:56:30 +0100

    -implement freeplaycontroller and view

---
+++ src/main/java/com/example/decipherjourney/Controller/FreePlayController.java
@@ -0,0 +1,63 @@
+package com.example.decipherjourney.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.decipherjourney.Model.*;
+import com.example.decipherjourney.Service.CookieService;
+import com.example.decipherjourney.Service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+
+/**
+ * FreePlayController is a menu controller that allows users to select a cipher type and learn and train.
+ * 
+ * Author: Oskar Schiedewitz
+ */
+@Controller
+public class FreePlayController {
+
+    /**
+     * Attribute to connect the view to the user database
+     */ 
+    @Autowired
+    UserService userService;
+
+    /**
+     * Attribute to safe the current cookie.
+     */
+    @Autowired
+    CookieService cookieService;
+
+    /**
+     * Function to freeplay the login view.
+     * 
+     * @param request   HTTP request made by a client.
+     * @param model     The model to set attributes for the view.
+     * 
+     * @return The view name for the free play menu.
+     */
+    @RequestMapping("/freeplay")
+    public String showFreePlayMenu(HttpServletRequest request, Model model) {
+        System.out.println("Switched to freeplay page.");
+        
+        // Check if user is logged in if not switch to login page where he can login or register
+
+        if (!cookieService.isCookieSet(request, "user-cookie")) {
+            System.out.println("Not logged in yet switch to login page.");
+            return "redirect:/login";
+        } else {
+            try {
+                User user = userService.getCurrentUser(request);
+                model.addAttribute("username", user.getUsername());
+                System.out.println("Cookie was succesful.");
+    
+            } catch (Exception e) {
+                System.out.println("Cookie was not succesful.");
+            }
+        }
+
+        return "freeplay";
+    }
+
+}
