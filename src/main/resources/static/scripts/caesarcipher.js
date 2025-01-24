d35fdff517fdcd90e6afde327b2e3eb58b957afe
Oskar Schiedewitz <schiedewitz1@uni-potsdam.de>
2025-01-24 16:56:59 +0100

    -implementing exampel in caesarcipher

--- src/main/resources/static/scripts/caesarcipher.js
+++ src/main/resources/static/scripts/caesarcipher.js
@@ -1,4 +1,3 @@
-
 const diskContainer = document.getElementById('disk-container');
 const innerDisk = document.getElementById('inner-disk');
 
@@ -54,4 +53,23 @@ document.addEventListener('DOMContentLoaded', () => {
   // Apply the initial rotation
   const currentAngle = spinDegree;
   innerDisk.style.transform = `rotate(${currentAngle}deg)`;
+});
+
+// Controlling the modal
+
+const openModalBtn = document.getElementById('open-modal-btn');
+const tutorialModal = document.getElementById('tutorial-modal');
+const closeModalBtn = document.getElementById('close-modal-btn');
+const video = document.getElementById('video');
+
+openModalBtn.addEventListener('click', () => {
+    tutorialModal.style.display = 'block'; 
+    video.play(); 
+});
+
+// Close the modal
+closeModalBtn.addEventListener('click', () => {
+  tutorialModal.style.display = 'none';
+  video.pause(); 
+  video.currentTime = 0; 
 });
